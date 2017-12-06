77c74249d21a5bb0c53ea0bb714e8680404cc39d
jagodarybacka <jagoda.roza.rybacka@gmail.com>
2017-12-06 16:27:59 +0100

    nondet transitions

---
+++ automata/nondeterministic/Automata.js
@@ -0,0 +1,52 @@
+'use strict';
+
+class Automata {
+  constructor() {
+    this.state = 0;
+    this.transitions = [];
+    this.accepting = [];
+  }
+
+  resetState() {
+    this.state = 0;
+  }
+
+  route(from, to, char) {
+    if(!to) {
+      if (!this.transitions[from]) {
+        this.transitions[from] = {};
+      }
+      this.accepting.push(from);
+    } else {
+      if (!this.transitions[from]) {
+        let arr = [to]
+        this.transitions[from] = {[char]: arr}
+      } else if (!this.transitions[from][char]){
+        this.transitions[from][char] = [to];
+      } else {
+        this.transitions[from][char].push(to);
+      }
+    }
+  }
+
+  check(str) {
+    for (let v of str) {
+      if (this.transitions[this.state][v]) {
+        this.state = this.transitions[this.state][v]
+      } else {
+          this.resetState()
+          return false
+      }
+    }
+
+    if (this.accepting.indexOf(this.state.toString()) >= 0) {
+        this.resetState()
+        return true
+    } else {
+      this.resetState()
+      return false
+    }
+  }
+}
+
+module.exports = Automata
---
+++ automata/nondeterministic/index.js
@@ -0,0 +1,65 @@
+'use strict'
+const readline = require('readline')
+const fs = require('fs')
+const Automata = require('./Automata.js')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+})
+
+function app() {
+  return new Promise(
+    (resolve, reject) => {
+      const aut = new Automata()
+      readInputFile()
+        .then(res => {
+            return res.map((el) => aut.route(...el))
+        })
+        .then(readArgvFile)
+        .then(data => data.split('\n'))
+        .then(res => {
+          resolve({words: res, aut})
+        })
+    }
+  )
+}
+
+function readInputFile() {
+  return new Promise(
+      (resolve, reject) => {
+        let arr = []
+        rl.on('line', line => {
+          if (line[0] !== "#" && line.length) {
+            arr.push(line.split(' '))
+            resolve(arr)
+          }
+        })
+    }
+  )
+}
+
+function readArgvFile() {
+  return new Promise(
+    (resolve, reject) => {
+      fs.readFile(process.argv[2], 'utf8',
+        (err, data) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(data)
+          }
+        })
+    })
+}
+
+app()
+  .then(res => {
+    console.log(res.aut.transitions);
+    // res.words.forEach((str) => {
+    //     if (str.length > 0) {
+    //       console.log(res.aut.check(str) ? 'YES' : 'NO', str)
+    //     }
+    // });
+  });
